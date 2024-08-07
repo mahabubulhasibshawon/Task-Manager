0f6dc540049254c20ec67da0000573e72ead80d7
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-08-07 13:00:33 +0600

    add tik sign

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,50 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/src/homepage_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final controller = HomepageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Manager', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+        title: const Text(
+          'Task Manager',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Colors.purple,
       ),
-      
       body: ListView.builder(
           itemCount: controller.taskList.length,
-          itemBuilder: (ctx, index){
-        return ExpansionTile(
-          title: Text(
-              controller.taskList[index].taskName,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          leading: Icon(Icons.list_sharp),
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Row(
+          itemBuilder: (ctx, index) {
+            return ExpansionTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    controller.taskList[index].taskDetails,
-                    style: const TextStyle(
-                      fontSize: 16
-                    ),
-                    textAlign: TextAlign.start,
+                    controller.taskList[index].taskName,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
+                  Icon(Icons.done,color: Colors.green,)
                 ],
               ),
-            )
-          ],
-        );
-      }),
+              leading: const Icon(Icons.list_sharp),
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Text(
+                        controller.taskList[index].taskDetails,
+                        style: const TextStyle(fontSize: 16),
+                        textAlign: TextAlign.start,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: const Icon(Icons.add, color: Colors.white,),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (ctx) {
+                return AlertDialog(
+                  title: const Text(
+                    'Add new Task',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        controller: controller.taskTitleController,
+                        decoration: InputDecoration(
+                            label: Text('Task Name'),
+                            hintText: 'Name of the task'),
+                      ),
+                      TextField(
+                        controller: controller.taskDetailsController,
+                        decoration: InputDecoration(
+                            label: Text('Details'),
+                            hintText: 'Type your task details'),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: (){
+                          setState(() {
+                            controller.addNewTask();
+                          });
+                        },
+                        child: const Text(
+                          'Add',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ))
+                  ],
+                );
+              });
+        },
         backgroundColor: Colors.purple.shade700,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
--- lib/src/homepage_controller.dart
+++ lib/src/homepage_controller.dart
@@ -1,9 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
 import '../models/task_model.dart';
 
-class HomepageController{
+class HomepageController {
+  final TextEditingController taskTitleController = TextEditingController();
+  final TextEditingController taskDetailsController = TextEditingController();
+
   List<TaskModel> taskList = [
-    TaskModel(taskName: 'demo task', taskDetails: 'Hello world\n', isComplete: false),
+    TaskModel(
+        taskName: 'demo task', taskDetails: 'Hello world\n', isComplete: false),
     TaskModel(taskName: 'demo task', taskDetails: '', isComplete: false),
     TaskModel(taskName: 'demo task', taskDetails: '', isComplete: false),
   ];
+
+  void addNewTask() {
+    taskList.add(TaskModel(
+        taskName: taskDetailsController.text,
+        taskDetails: taskDetailsController.text,
+        isComplete: false));
+  }
 }
